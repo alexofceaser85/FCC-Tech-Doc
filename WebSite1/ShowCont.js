47949493c2d85bd5ece3036e046f2db18cf647ac
Alex <alexofcesare@gmail.com>
2018-09-19 06:18:32 -0400

    Technical Documentation Page for Free Code Camp

---
+++ WebSite1/ShowCont.js
@@ -0,0 +1,5 @@
+﻿function ShowCont() {
+    document.getElementById("nav-list").classList.toggle("ShowNavBar");
+    document.getElementById("ShowNav").classList.toggle("ChangeBtnSize");
+    document.getElementById("main-doc").classList.toggle("ShiftBody");
+}
